353a24f391f74a5054a581ecc04faf8bfe9fdc5c
bruno montebello <brunormontebello@hotmail.com>
2020-05-01 22:53:42 -0300

    Create scrapper method

--- scrapper.rb
+++ scrapper.rb
@@ -3,9 +3,9 @@ require 'httparty'
 require 'byebug'
 
 def scrapper
-  url = "https://ec.europa.eu/digital-single-market/en"
+  url = "https://www.itjobs.pt/emprego?type=1&location=14"
   unparsed_page = HTTParty.get(url) #give us back the raw html of the page
-  parsed_page = Nokogiri::HTTP(unparsed_page)
+  parsed_page = Nokogiri::HTML(unparsed_page)
   byebug
 end
 
